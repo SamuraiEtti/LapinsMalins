cbf52bb0e9b3b397931f8df0c7a809c357c28ec8
SamuraiEtti <samurai.etti@gmail.com>
2016-06-03 11:41:52 +0200

    mini maj

--- Script/barreRecherche.js
+++ Script/barreRecherche.js
@@ -1,37 +1,39 @@
-$(function(){
-    $("#barreRecherche").on("input",chercherTshirt);
-    $("#boutonAfficher").on("click",nombreTshirt);
-    function chercherTshirt(e){
-      
-        var lettre=$(this).val();
-        $.getJSON("Controller/barreRecherche.php",{
-            nom:lettre
-        },function(data){
+$(function () {
+    $("#barreRecherche").on("input", chercherTshirt);
+    $("#boutonAfficher").on("click", nombreTshirt);
+
+    function chercherTshirt(e) {
+
+        var lettre = $(this).val();
+        $.getJSON("Controller/barreRecherche.php", {
+            nom: lettre
+        }, function (data) {
             //console.log("couc");
-              $("#barreRecherche").text(data.nom);
-            $ul=$('#resultatTshirt');
+            $("#barreRecherche").text(data.nom);
+            $ul = $('#resultatTshirt');
             $ul.html('');
-             for(var i=0;i<data.length;i++){
-              $("<li/>").text(data[i].nom)
-              .attr("class","tshirtResultat")
-                  .appendTo($ul);
-          }
-            
-          
+            for (var i = 0; i < data.length; i++) {
+                $("<li/>").text(data[i].nom)
+                    .attr("class", "tshirtResultat")
+                    .appendTo($ul);
+            }
+
+
         })
-      nombreTshirt();
- 
+        nombreTshirt();
+
     }
-      function nombreTshirt(){
-          setTimeout(function(){
-               var nombreli= $("#resultatTshirt>li");
-             var t_shirt=nombreli.length;
-              if(t_shirt<=1){
-                   $("#nombreTshirt").text(t_shirt+" tshirt");
-              }else{
-                   $("#nombreTshirt").text(t_shirt+" tshirts");
-              }
-          },200)
-         
+
+    function nombreTshirt() {
+        setTimeout(function () {
+            var nombreli = $("#resultatTshirt>li");
+            var t_shirt = nombreli.length;
+            if (t_shirt <= 1) {
+                $("#nombreTshirt").text(t_shirt + " tshirt");
+            } else {
+                $("#nombreTshirt").text(t_shirt + " tshirts");
+            }
+        }, 200)
+
     }
 })
