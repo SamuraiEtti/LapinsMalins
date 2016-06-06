d6fa83201d5396fb8597cfb3ed52dcdbe3e3719d
SamuraiEtti <samurai.etti@gmail.com>
2016-06-06 16:39:49 +0200

    Ajout formulaire d'ajout de tshirt (pas encore fonctionnel)

--- Script/client.js
+++ Script/client.js
@@ -1,19 +1,19 @@
 $(function () {
-  
-    $("")
-    
-    
+
+    $("#boutonAjout").on("click", launchFormAjout);
+
+
     function listeTeeshirt() {
         $.ajax("Controller/listeTeeshirt.php", {
             success: function (d, s, xhr) {
                 //console.log("success : ", d);
                 for (var i = 0; i < d.length; i++) {
                     $("#resultatTshirt").append(
-                        $("<li/>").text(d[i]["nom"] + " - " + d[i]["createur"]).attr("class","tshirtResultat")
-                        .attr("data-id",d[i]["id"])
-                         .append($("<input type='button' value='X' name='delete' class='suppression'/>"))
-              .append($("<input type='button' value='m' name='modif' class='modification'/>"))
-                 .append($("<input type='button' value='+' name='details' class='detail'/>"))
+                        $("<li/>").text(d[i]["nom"] + " - " + d[i]["createur"]).attr("class", "tshirtResultat")
+                        .attr("data-id", d[i]["id"])
+                        .append($("<input type='button' value='X' name='delete' class='suppression'/>"))
+                        .append($("<input type='button' value='m' name='modif' class='modification'/>"))
+                        .append($("<input type='button' value='+' name='details' class='detail'/>"))
                     )
                 }
             },
@@ -22,11 +22,25 @@ $(function () {
             }
         })
     }
-    
+
     function searchTeeshirt() {
-        
+
     }
-    
+
+    function launchFormAjout(e) {
+        var $bouton = $('#boutonAjout');
+        var $elm = $bouton.parent();
+        $("<div/>").attr("id", "ajoutForm").appendTo($elm);
+        var $div = $("#ajoutForm");
+        if ($div.html() !== "") {
+            $div.html("");
+            $('#fermerAjout').attr('id', 'boutonAjout');
+        } else {
+            $div.load("Template/ajout.html");
+            $bouton.attr('id', "fermerAjout");
+        }
+    }
+
     // liste tous les tshirts une fois par défaut
     listeTeeshirt();
 })
