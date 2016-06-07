e683c9028cd27e606975c27434c22478be1d5752
internet11 <internet11>
2016-06-07 16:42:27 +0200

    details v2

--- Script/detail.js
+++ Script/detail.js
@@ -1,26 +1,29 @@
 $(function () {
 
     $(document).on("click", ".detail", afficheDetail);
-    console.log("test");
-
+    $(document).on("click", ".moins", cacheDetail);
     function afficheDetail(e) {
-          if($(e.target==".detail")&&($("#divDetail"))){
-            console.log("c'est le bouton")
-        }
-        console.log("ca passe!");
-  var leClick=e.target;
-      $(leClick).css("background-image","url(images/minus.png)");
+    
+ 
+        var leClick = e.target;
+        //$(leClick).css("background-image", "url(images/minus.png)");
+        $(leClick).removeClass("detail");
+        $(".moins").removeClass("moins").addClass("detail");
+        $(leClick).addClass("moins");
+        
         var papa = $(e.target).parent();
         var idTshirt = papa.attr("data-id");
         //console.log(idTshirt);
-        var leDiv = $("<div/>").attr("class", "ajoutForm");
-        papa.append(leDiv);
-        leDiv.load("Template/detail.html");
+       var $lesDiv=$(".ajoutForm").remove();
+        
+        var $leDiv = $("<div/>").attr("class", "ajoutForm");
+                    papa.append($leDiv);
+        $leDiv.load("Template/detail.html");
         $.getJSON("Controller/detailController.php", {
             id_tshirt: idTshirt
         }, function (data) {
             //selectionnet chaque champ du formulaire et lui dsonner la vlauer renvoyée par le json
-//            console.log(data);
+            //            console.log(data);
             $("#detailNom").text(data[0].nom);
             $("#detailPrix").text(data[0].prix);
             $("#detailDate").text(data[0].date);
@@ -28,13 +31,30 @@ $(function () {
             $("#detailCrea").text(data[0].createur);
             $("#detailMat").text(data[0].matiere);
             $("#detailCat").text(data[0].categorie);
-            $("#imgListe").attr("src","images/tshirt/" +data[0].imgListe);
-              $("#imgDetail").attr("src","images/tshirt/" +data[0].imgDetails);
+            $("#imgListe").attr("src", "images/tshirt/" + data[0].imgListe);
+            $("#imgDetail").attr("src", "images/tshirt/" + data[0].imgDetails);
         })
-      
+        
+
+        
+        
+
     }
-    
-    function cacheDetail(e){
-        console.log("yo");
+
+    function cacheDetail(e) {
+       // console.log("yo");
+       
+        var papaDiv=$(e.target).parent();
+        var idLi= papaDiv.attr("data-id");
+        console.log(idLi);
+      
+        var $div=$("li[data-id="+idLi+"] .ajoutForm");
+        if($div.html()!==""){
+            $div.remove();
+            var le_clic=$(e.target);
+           le_clic.removeClass("moins");
+            le_clic.addClass("detail");
+        }
+        
     }
 })
